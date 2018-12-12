1d01c9b2332fea9382a2dbdec64516a314657f6e
Denise Martini <denise.martini1406@gmail.com>
2018-12-13 11:38:27 +1300

    added random seed to admixture script

--- admixture.sh
+++ admixture.sh
@@ -1,11 +1,16 @@
+#!/bin/bash -e
 
+## 13.12.18, Denise
+# fixed from a previous script, in order to run with different random seeds as well as different Ks
 # set variables
 admixture=/usr/local/admixture_1.3.0/admixture
+seedlist="76 11 24 161 358"
 Klist="1 2 3 4 5 6 7 8 9 10"
 
-for K in $Klist
+for s in $seedlist
 do
-
-$admixture --cv ../biallelic_common_snps.bed $K -j8 | tee log${K}.out
-
+  for K in $Klist
+  do
+    $admixture -s $s --cv maxmiss90_common_snps.ped $K -j8 | tee admix_${s}_${K}.log
+  done
 done
